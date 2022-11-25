c023f81cdbf917ae639628712314115dfd8b9560
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-25 16:27:32 -0300

    07 - Requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <h1>
+          Sistema Solar
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Missions;
